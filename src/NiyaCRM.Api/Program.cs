9b86ed33d732c32dc89dbb1ee220e3966005f864
Nithin Prathapan <nithinp89@gmail.com>
2025-07-17 01:26:23 +0200

    Serilog

--- src/NiyaCRM.Api/Program.cs
+++ src/NiyaCRM.Api/Program.cs
@@ -1,5 +1,53 @@
+using Microsoft.AspNetCore.HttpLogging;
+using NiyaCRM.Api.Configurations;
+using NiyaCRM.Infrastructure.Logging.Serilog;
+using Serilog;
+using System.Reflection;
+
+//var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddConfigurations();
+
+Log.Error("Server Booting Up...");
+
+// HTTP Loging Config
+builder.Services.AddHttpLogging(logging =>
+{
+    logging.LoggingFields = HttpLoggingFields.All;
+    logging.RequestHeaders.Add("x-correlation-id");
+    logging.RequestHeaders.Add("X-Forwarded-For");
+    logging.RequestHeaders.Add("X-Forwarded-Proto");
+    logging.RequestHeaders.Add("X-Forwarded-Port");
+    logging.RequestHeaders.Add("X-Forwarded-Host");
+    logging.RequestHeaders.Add("X-Forwarded-Server");
+    logging.RequestHeaders.Add("X-Amzn-Trace-Id");
+    logging.RequestHeaders.Add("Upgrade-Insecure-Requests");
+    logging.RequestHeaders.Add("sec-ch-ua");
+    logging.RequestHeaders.Add("sec-ch-ua-mobile");
+
+    logging.ResponseHeaders.Add("x-correlation-id");
+    logging.ResponseHeaders.Add("Pragma");
+    logging.ResponseHeaders.Add("Cache-Control");
+    logging.ResponseHeaders.Add("max-age");
+});
+
+builder.Services.AddControllers();
+
+// For http request context accessing
+builder.Services.AddHttpContextAccessor();
+
+// Register Serilog using the extension method
+builder.Host.RegisterSerilog();
+
 var app = builder.Build();
+
+// Log all requests and response.
+app.UseHttpLogging();
+
+// Streamlines framework logs into a single message per request, including path, method, timings, status code, and exception.
+app.UseSerilogRequestLogging();
 
 app.MapGet("/", () => "Hello World!");
 
---
+++ src/NiyaCRM.Infrastructure/Logging/Serilog/LoggerSettings.cs
@@ -0,0 +1,22 @@
+namespace NiyaCRM.Infrastructure.Logging.Serilog
+{
+    public class LoggerSettings
+    {
+        public bool WriteToFile { get; set; }
+        public string LogFilePath { get; set; } = "logs/";
+        public bool CompactConsoleLogging { get; set; }
+        public string? ElasticSearchUrl { get; set; }
+        public LogLevelSettings LogLevel { get; set; } = new();
+
+        public class LogLevelSettings
+        {
+            public string Default { get; set; } = "Information";
+            public string? Microsoft { get; set; } = "Warning";
+            public string? MicrosoftAspNetCore { get; set; } = "Warning";
+            public string? MicrosoftHostingLifetime { get; set; } = "Warning";
+            public string? MicrosoftAspNetCoreAuthentication { get; set; } = "Warning";
+            public string? MicrosoftAspNetCoreMvcInfrastructure { get; set; } = "Warning";
+            public string? MicrosoftEntityFrameworkCore { get; set; } = "Warning";
+        }
+    }
+}
