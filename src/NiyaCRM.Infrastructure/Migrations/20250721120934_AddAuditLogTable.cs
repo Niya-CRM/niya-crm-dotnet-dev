42ef05d5414ead9f01e75759398c49e35f5a63e8
Nithin Prathapan <nithinp89@gmail.com>
2025-07-22 00:23:05 +0200

    migration files

---
+++ src/NiyaCRM.Infrastructure/Migrations/20250721120934_AddAuditLogTable.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NiyaCRM.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuditLogTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "audit_logs",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    module = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    @event = table.Column<string>(name: "event", type: "character varying(100)", maxLength: 100, nullable: false),
+                    mapped_id = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    ip = table.Column<string>(type: "character varying(45)", maxLength: 45, nullable: true),
+                    data = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    created_by = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_audit_logs", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_audit_logs_module_mapped_id_created_at",
+                table: "audit_logs",
+                columns: new[] { "module", "mapped_id", "created_at" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "audit_logs");
+        }
+    }
+}
